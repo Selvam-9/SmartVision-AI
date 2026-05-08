4ef9e62e3c3526196422c5b1a0710ed3325cafe9
Selvam <selvam.anandhan@idp.com>
2026-05-08 10:55:42 +0530

    Refactor RTCConfiguration for NAT traversal

--- app/app_pages/page_webcam.py
+++ app/app_pages/page_webcam.py
@@ -93,14 +93,9 @@ def show():
         ctx = webrtc_streamer(
             key="yolo-live",
             video_processor_factory=YOLOVideoProcessor,
-            rtc_configuration=RTCConfiguration({
-                "iceServers": [
-                    {"urls": ["stun:stun.l.google.com:19302"]},
-                    {"urls": ["stun:stun1.l.google.com:19302"]},
-                    {"urls": ["stun:stun2.l.google.com:19302"]},
-                    {"urls": ["stun:stun.services.mozilla.com"]},
-                ]
-            }),
+            rtc_configuration=RTCConfiguration(
+                {"iceServers": [{"urls": ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302", "stun:stun.services.mozilla.com"]}]}
+            ),
             media_stream_constraints={"video": True, "audio": False},
         )
 
